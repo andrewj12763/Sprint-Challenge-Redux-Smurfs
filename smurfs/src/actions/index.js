ae1ede28555c2b9e194406ff04058ab992994420
slaforsjov <andrewj12763@gmail.com>
2019-07-21 18:41:21 -0600

    working on delete error

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,45 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios'
+
+export const FETCHING_SMURFS = "FETCHING_SMURFS"
+export const SUCCESS = "SUCCESS"
+export const ERROR = "ERROR"
+export const ADDING_SMURF = "ADDING_SMURF"
+export const DELETING_SMURF = "DELETING_SMURF"
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS })
+  axios.get('http://localhost:3333/smurfs')
+  .then(res => {
+    console.log(res)
+    dispatch({ type: SUCCESS, payload: res.data })
+  })
+  .catch(err => {
+    console.log(err)
+    dispatch({ type: ERROR, payload: err })
+  })
+}
+
+export const addSmurf = (smurf) => dispatch => {
+  dispatch({ type: ADDING_SMURF })
+  axios.post('http://localhost:3333/smurfs', smurf) 
+  .then(res => {
+    dispatch({ type: SUCCESS, payload: res.data })
+  })
+  .catch(err => {
+    dispatch({ type: ERROR, payload: err })
+  })
+}
+
+export const deleteSmurf = (id) => dispatch => {
+  dispatch({ type: DELETING_SMURF })
+  axios.delete(`http://localhost:3333/smurfs/${id}`)
+  .then(res => {
+    dispatch({ type: SUCCESS, payload: res.data})
+  })
+  .catch(err => {
+    dispatch({ type: ERROR, payload: err })
+  })
+} 
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import Smurf from './Smurf'
+import SmurfForm from './SmurfForm'
 import './App.css';
+
+import { connect } from 'react-redux'
+import { getSmurfs } from '../actions'
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
+
+  
+  componentDidMount() {
+    this.props.getSmurfs()
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +27,29 @@ class App extends Component {
         <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        
+        <p>
+          {this.props.smurfs.map(smurf => {
+          return <Smurf smurf={smurf} key={smurf.name} />
+          })}
+        </p>
+
+        <SmurfForm />
+
       </div>
     );
   }
 }
 
-export default App;
+function mstp(state) {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    addingSmurf: state.addingSmurf,
+    updatingSmurf: state.updatingSmurf,
+    deletingSmurf: state.deletingSmurf,
+    error: state.error
+  }
+}
+
+export default connect(mstp, { getSmurfs })(App)
